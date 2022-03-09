0792d37d67756d719d4ff494dbb0eb7bb3b91e72
Carl Andrew Castañas <89068725+carlcastanas@users.noreply.github.com>
2022-03-09 17:37:56 +0800

    Update conversion.cpp

--- conversion.cpp
+++ conversion.cpp
@@ -1,3 +1,7 @@
+// Author: M. Merculio
+// Date: March 9, 2022
+// Description: Conversion Program
+
 #include <iostream>	 // for cin, cout
 #include <string>
 #include <cmath>
@@ -21,7 +25,7 @@ cin>>init_unit;
 if (init_unit != "mm" && init_unit != "cm" && init_unit != "km" )
 {
 cerr<<"--> Sorry, unit to convert from is invalid\n"
-<<"Thank you for using Topiloe Metric Unit converter Program\n";
+<<"Thank you for using Metric Unit converter Program\n";
 }
 else 
 {
@@ -32,105 +36,105 @@ cin>>sec_unit;
 if (sec_unit != "mm" && sec_unit != "cm" && sec_unit != "m" && sec_unit != "km")
 {
 cout<<"--> Sorry, unit to convert TO is invalid\n"
-<<"Thank you for using Topiloe Metric Unit converter Program\n"; 
+<<"Thank you for using Metric Unit converter Program\n"; 
 }
 else if (init_unit == "mm" && sec_unit == "cm")
 {
 cout<<"Enter numerical value to convert: \n";
 cin>>conv_val;
 cout<<"Result of conversion: "<<conv_val<<" is equivalent to "<<conv_val*10<<" "<<sec_unit<<endl //cm=mm*10
-<<"Thank you for using Topiloe Metric Unit converter Program\n";
+<<"Thank you for using Metric Unit converter Program\n";
 }
 else if (init_unit == "mm" && sec_unit == "m")
 {
 cout<<"Enter numerical value to convert: \n";
 cin>>conv_val;
 cout<<"Result of conversion: "<<conv_val<<" is equivalent to "<<conv_val*100<<" "<<sec_unit<<endl //m=mm*100
-<<"Thank you for using Topiloe Metric Unit converter Program\n";
+<<"Thank you for using Metric Unit converter Program\n";
 }
 else if (init_unit == "mm" && sec_unit == "km")
 {
 cout<<"Enter numerical value to convert: \n";
 cin>>conv_val;
 cout<<"Result of conversion: "<<conv_val<<" is equivalent to "<<conv_val*100000<<" "<<sec_unit<<endl //km=100000*mm
-<<"Thank you for using Topiloe Metric Unit converter Program\n";
+<<"Thank you for using Metric Unit converter Program\n";
 }
 else if (init_unit == "cm" && sec_unit == "mm")
 {
 cout<<"Enter numerical value to convert: \n";
 cin>>conv_val;
 cout<<"Result of conversion: "<<conv_val<<" is equivalent to "<<conv_val/10<<" "<<sec_unit<<endl //cm=mm*10
-<<"Thank you for using Topiloe Metric Unit converter Program\n";
+<<"Thank you for using Metric Unit converter Program\n";
 }
 else if (init_unit == "cm" && sec_unit == "m")
 {
 cout<<"Enter numerical value to convert: \n";
 cin>>conv_val;
 cout<<"Result of conversion: "<<conv_val<<" is equivalent to "<<conv_val*10<<" "<<sec_unit<<endl //cm=m*10
-<<"Thank you for using Topiloe Metric Unit converter Program\n";
+<<"Thank you for using Metric Unit converter Program\n";
 }
 else if (init_unit == "cm" && sec_unit == "mm")
 {
 cout<<"Enter numerical value to convert: \n";
 cin>>conv_val;
 cout<<"Result of conversion: "<<conv_val<<" is equivalent to "<<conv_val/10<<" "<<sec_unit<<endl //cm=mm*10
-<<"Thank you for using Topiloe Metric Unit converter Program\n";
+<<"Thank you for using Metric Unit converter Program\n";
 }
 else if (init_unit == "cm" && sec_unit == "m")
 {
 cout<<"Enter numerical value to convert: \n";
 cin>>conv_val;
 cout<<"Result of conversion: "<<conv_val<<" is equivalent to "<<conv_val*10<<" "<<sec_unit<<endl //m=cm*10
-<<"Thank you for using Topiloe Metric Unit converter Program\n";
+<<"Thank you for using Metric Unit converter Program\n";
 }
 else if (init_unit == "cm" && sec_unit == "km")
 {
 cout<<"Enter numerical value to convert: \n";
 cin>>conv_val;
 cout<<"Result of conversion: "<<conv_val<<" is equivalent to "<<conv_val*10000<<" "<<sec_unit<<endl //km=10000*cm
-<<"Thank you for using Topiloe Metric Unit converter Program\n";
+<<"Thank you for using Metric Unit converter Program\n";
 }
 else if (init_unit == "m" && sec_unit == "mm")
 {
 cout<<"Enter numerical value to convert: \n";
 cin>>conv_val;
 cout<<"Result of conversion: "<<conv_val<<" is equivalent to "<<conv_val/100<<" "<<sec_unit<<endl //m=mm*100
-<<"Thank you for using Topiloe Metric Unit converter Program\n";
+<<"Thank you for using Metric Unit converter Program\n";
 }
 else if (init_unit == "m" && sec_unit == "cm")
 {
 cout<<"Enter numerical value to convert: \n";
 cin>>conv_val;
 cout<<"Result of conversion: "<<conv_val<<" is equivalent to "<<conv_val/10<<" "<<sec_unit<<endl //m=cm*10
-<<"Thank you for using Topiloe Metric Unit converter Program\n";
+<<"Thank you for using Metric Unit converter Program\n";
 }
 else if (init_unit == "m" && sec_unit == "km")	
 {
 cout<<"Enter numerical value to convert: \n";
 cin>>conv_val;
 cout<<"Result of conversion: "<<conv_val<<" is equivalent to "<<conv_val*1000<<" "<<sec_unit<<endl //km=1000*m
-<<"Thank you for using Topiloe Metric Unit converter Program\n";
+<<"Thank you for using Metric Unit converter Program\n";
 }
 else if (init_unit == "km" && sec_unit == "mm")	
 {
 cout<<"Enter numerical value to convert: \n";
 cin>>conv_val;
 cout<<"Result of conversion: "<<conv_val<<" is equivalent to "<<conv_val/100000<<" "<<sec_unit<<endl //km=100000*mm
-<<"Thank you for using Topiloe Metric Unit converter Program\n";
+<<"Thank you for using Metric Unit converter Program\n";
 }
 else if (init_unit == "km" && sec_unit == "cm")
 {
 cout<<"Enter numerical value to convert: \n";
 cin>>conv_val;
 cout<<"Result of conversion: "<<conv_val<<" is equivalent to "<<conv_val/10000<<" "<<sec_unit<<endl //km=10000*cm
-<<"Thank you for using Topiloe Metric Unit converter Program\n";
+<<"Thank you for using Metric Unit converter Program\n";
 }
 else if (init_unit == "km" && sec_unit == "m")
 {
 cout<<"Enter numerical value to convert: \n";
 cin>>conv_val;
 cout<<"Result of conversion: "<<conv_val<<" is equivalent to "<<conv_val/1000<<" "<<sec_unit<<endl //km=1000*m
-<<"Thank you for using Topiloe Metric Unit converter Program\n";
+<<"Thank you for using Metric Unit converter Program\n";
 }
 system("Pause");
 }
